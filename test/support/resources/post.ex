1e4e715b69d7a9f5147eaf46b21e34742aa5dd94
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2023-10-27 14:50:51 +0200

    wip

--- test/support/resources/post.ex
+++ test/support/resources/post.ex
@@ -91,12 +91,11 @@ defmodule AshGraphql.Test.Post do
   @moduledoc false
   alias AshGraphql.Test.Comment
   alias AshGraphql.Test.SponsoredComment
-  alias AshGraphql.Test.PubSub
 
   use Ash.Resource,
     data_layer: Ash.DataLayer.Ets,
     authorizers: [Ash.Policy.Authorizer],
-    extensions: [AshGraphql.Resource, Ash.Notifier.PubSub]
+    extensions: [AshGraphql.Resource]
 
   require Ash.Query
 
@@ -166,14 +165,6 @@ defmodule AshGraphql.Test.Post do
       # this is a mutation just for testing
       action(:random_post, :random)
     end
-  end
-
-  pub_sub do
-    module(PubSub)
-    prefix("post")
-    broadcast_type(:notification)
-
-    publish_all(:create, "created")
   end
 
   actions do
